53cc30d315f9eca9dbfdb880654894f427aafbbb
Simon Albright <simon.albright@cern.ch>
2019-08-20 11:35:29 +0200

    added error for when inner potential wells expected

--- utilities/Exceptions.py
+++ utilities/Exceptions.py
@@ -1,5 +1,15 @@
+###########################
+#####BASIC DATA ERRORS#####
+###########################
 class InputError(Exception):
     pass
 
 class DataDefinitionError(Exception):
+    pass
+
+
+###############################
+#####POTENTIAL WELL ERRORS#####
+############################### 
+class No_Inner_Potential_Well(Exception):
     pass
