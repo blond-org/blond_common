c8df021dc68781a0e358d87ca9b3d9d88da08466
alasheen <alexandre.lasheen@cern.ch>
2019-10-25 11:56:14 +0200

    Adding FWHM unittests

--- __TESTS/unittests/fitting/test_profile.py
+++ __TESTS/unittests/fitting/test_profile.py
@@ -31,8 +31,9 @@ if os.path.abspath(this_directory + '../../../../') not in sys.path:
     sys.path.insert(0, os.path.abspath(this_directory + '../../../../'))
 
 from blond_common.interfaces.beam.analytic_distribution import (
-    gaussian, parabolicAmplitude, binomialAmplitudeN, _binomialRMS)
-from blond_common.fitting.profile import RMS
+    Gaussian, parabolicAmplitude, binomialAmplitudeN, _binomial_full_to_rms,
+    _binomial_full_to_fwhm)
+from blond_common.fitting.profile import RMS, FWHM
 
 
 class TestFittingProfile(unittest.TestCase):
@@ -52,9 +53,10 @@ class TestFittingProfile(unittest.TestCase):
         self.length_gauss = 2e-9
         self.initial_params_gauss = [self.amplitude_gauss, self.position_gauss,
                                      self.length_gauss]
-        self.gaussian_dist = gaussian(self.time_array,
-                                      *self.initial_params_gauss)
+        self.gaussian_dist = Gaussian(*self.initial_params_gauss).profile(
+            self.time_array)
         self.sigma_gauss = self.length_gauss
+        self.fwhm_gauss = Gaussian(*self.initial_params_gauss).FWHM
 
         self.amplitude_parabamp = 1.3
         self.position_parabamp = 4e-9
@@ -64,7 +66,8 @@ class TestFittingProfile(unittest.TestCase):
                                         self.length_parabamp]
         self.parabamp_dist = parabolicAmplitude(self.time_array,
                                                 *self.initial_params_parabamp)
-        self.sigma_parabamp = _binomialRMS(self.length_parabamp, 1.5)
+        self.sigma_parabamp = _binomial_full_to_rms(self.length_parabamp, 1.5)
+        self.fwhm_parabamp = _binomial_full_to_fwhm(self.length_parabamp, 1.5)
 
         self.amplitude_binom = 0.77
         self.position_binom = 18.3e-9
@@ -74,15 +77,19 @@ class TestFittingProfile(unittest.TestCase):
                                      self.length_binom, self.exponent_binom]
         self.binom_dist = binomialAmplitudeN(self.time_array,
                                              *self.initial_params_binom)
-        self.sigma_binom = _binomialRMS(self.length_binom, self.exponent_binom)
+        self.sigma_binom = _binomial_full_to_rms(self.length_binom,
+                                                 self.exponent_binom)
+        self.fwhm_binom = _binomial_full_to_fwhm(self.length_binom,
+                                                 self.exponent_binom)
 
     # Tests for RMS -----------------------------------------------------------
     '''
-    Testing the RMS function on the three, the absolute precision required
-    was set manually for the time being.
+    Testing the RMS function on the three profiles, the absolute precision
+    required was set manually for the time being.
 
     The test consists of 6 assertions comparing the mean and rms obtained
-    from the RMS function compared to the input for the 3 profiles.
+    from the RMS function compared to the analytical expectation for the
+    3 profiles.
 
     TODO: the precision is set manually atm and should be reviewed
 
@@ -128,6 +135,62 @@ class TestFittingProfile(unittest.TestCase):
 
         np.testing.assert_almost_equal(
             rms_binom*1e9, self.sigma_binom*1e9, decimal=6)
+
+    # Tests for FWHM ----------------------------------------------------------
+    '''
+    Testing the FWHM function on the three profiles, the absolute precision
+    required was set manually for the time being.
+
+    The test consists of 6 assertions comparing the center and fwhm obtained
+    from the FWHM function compared to the the analytical expectation for the
+    3 profiles.
+
+    TODO: the precision is set manually atm and should be reviewed
+
+    '''
+
+    def test_FWHM_gauss(self):
+        '''
+        Checking the center,fwhm obtained from FWHM function for the Gaussian
+        profile.
+        '''
+
+        center_gauss, fwhm_gauss = FWHM(self.time_array, self.gaussian_dist)
+
+        np.testing.assert_almost_equal(
+            center_gauss*1e9, self.position_gauss*1e9, decimal=8)
+
+        np.testing.assert_almost_equal(
+            fwhm_gauss*1e9, self.fwhm_gauss*1e9, decimal=3)
+
+    def test_FWHM_parabamp(self):
+        '''
+        Checking the center,fwhm obtained from FWHM function for the Parabolic
+        Amplitude profile.
+        '''
+
+        center_parabamp, fwhm_parabamp = FWHM(self.time_array,
+                                              self.parabamp_dist)
+
+        np.testing.assert_almost_equal(
+            center_parabamp*1e9, self.position_parabamp*1e9, decimal=8)
+
+        np.testing.assert_almost_equal(
+            fwhm_parabamp*1e9, self.fwhm_parabamp*1e9, decimal=3)
+
+    def test_FWHM_binom(self):
+        '''
+        Checking the center,fwhm obtained from FWHM function for a Binomial
+        profile.
+        '''
+
+        center_binom, fwhm_binom = FWHM(self.time_array, self.binom_dist)
+
+        np.testing.assert_almost_equal(
+            center_binom*1e9, self.position_binom*1e9, decimal=8)
+
+        np.testing.assert_almost_equal(
+            fwhm_binom*1e9, self.fwhm_binom*1e9, decimal=3)
 
 
 if __name__ == '__main__':
