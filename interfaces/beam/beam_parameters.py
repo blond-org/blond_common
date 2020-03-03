aceced311eab49a68fe24e2f17b48e51ccc54b15
Simon Albright <simon.albright@cern.ch>
2020-03-03 20:31:44 +0100

    tracking synchronous

--- interfaces/beam/beam_parameters.py
+++ interfaces/beam/beam_parameters.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # BLonD_Common imports
 from ...rf_functions import potential as pot
+from ...maths import calculus as calc
 
 
 class Beam_Parameters:
@@ -92,7 +93,6 @@ class Beam_Parameters:
         rfPars = self.rf.parameters_at_sample(sample)
         
         timeBounds = (0, ringPars['t_rev']/self.harmonic_divide)
-        print("TIMEBOUNDS:", timeBounds)
         
         if volts is None:
              vTime, vWave = pot.rf_voltage_generation(self.potential_resolution,
@@ -120,39 +120,42 @@ class Beam_Parameters:
     
     def track_synchronous(self, start_sample = 0):
         
-        #If no start points specified create a single particle at the lowest minimum
-        if self.particle_starts is None:
+        #If no start points specified create a single particle at the lowest 
+        #and leftest minimum
+        if self.init_coord is None:
             point = np.where(self.potential_well_array[0]
                              == np.min(self.potential_well_array[0]))[0][0]
-            self.particle_starts = [self.time_window_array[0][point]]
+            self.init_coord = [self.time_window_array[0][point]]
 
         if start_sample == 0:
-            self.particle_tracks = np.zeros([len(self.particle_starts),
+            self.particle_tracks = np.zeros([len(self.init_coord),
                                             self.n_samples])
 
-        self.n_particles = len(self.particle_starts)
+        self.n_particles = len(self.init_coord)
 
-        #Loop over all particles positioning them in closest minimum to declared start point
+        #Loop over all particles positioning them in closest minimum to 
+        #declared start point
         for p in range(self.n_particles):
 
             startPoint = np.where(self.time_window_array[0]
-                                  <= self.particle_starts[p])[0][-1]
+                                  <= self.init_coord[p])[0][-1]
 
             self.particle_tracks[p][0] = self.time_window_array[0][startPoint]
 
-            locs, values = calc.minmax_location(self.time_window_array[0],
-                                               self.potential_well_array[0])
+            locs, values = calc.minmax_location_cubic(self.time_window_array[0],
+                                                      self.potential_well_array[0])
             locs = locs[0]
             offsets = np.abs(self.particle_tracks[p][0] - locs)
             newLoc = np.where(offsets == np.min(offsets))[0][0]
                 
             self.particle_tracks[p][0] = locs[newLoc]
 
-        #Loop over all particles and all but first sample, at each sample new particle location is nearest minimum in potential well
+        #Loop over all particles and all but first sample, at each sample new 
+        #particle location is nearest minimum in potential well
         for p in range(self.n_particles):
             for t in range(start_sample+1, self.n_samples):
-               locs, values = calc.minmax_location(self.time_window_array[t], 
-                                                   self.potential_well_array[t])
+               locs, values = calc.minmax_location_cubic(self.time_window_array[t], 
+                                                         self.potential_well_array[t])
                locs = locs[0]
                offsets = np.abs(self.particle_tracks[p][t-1] - locs)
                newLoc = np.where(offsets == np.min(offsets))[0][0]
