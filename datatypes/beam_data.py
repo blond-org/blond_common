b8b56d3045aed065812367e5c23c036693fa6773
Simon Albright <simon.albright@cern.ch>
2020-05-11 15:23:14 +0200

    added detailed doc strings

--- datatypes/beam_data.py
+++ datatypes/beam_data.py
@@ -18,21 +18,39 @@ from ._core import _function, _expand_function, _check_time_turns,\
 
 class _beam_data(_function):
     """
-    Base class for defining beam data functions
+    Base class defining the data relevant to beam measurements
+
     Parameters
     ----------
     *args : float, 1D iterable of floats, 2D iterable of floats
+        The data defining the programs
     units : str
+        The units of the data
     time : iterable of floats
+        The time array to be used for the data, to be used if the time 
+        dependent data is not given as 2D array
     n_turns : int
+        The number of turns to be used, if a single value is given it will be
+        extended to n_turns length
     interpolation : str
+        The type of interpolation to be used
     **kwargs : keyword arguments
+        Additional kwargs used to define the data_type dict.
     
     Attributes
     ----------
-    As _function plus
+    data_type : dict
+        Dictionary containing relevant information to define the datatype.
+    timebase : str
+        Either 'single', 'by_turn', or 'by_time' depending on the definition
+        of the datatype.  As a string it is used as a key for the data_type 
+        dict.
     units : str
-    bunching : str
+        Identifies the units of the stored data.  Used as a string key in the
+        data_type dict.
+    interpolation : str
+        Identifier of the type of interpolation to be used when reshaping
+        the array, currently only 'linear' has been implemented.
     """
     
     def __new__(cls, *args, units, time = None, n_turns = None, 
@@ -50,7 +68,8 @@ class _beam_data(_function):
         if 'by_turn' in data_types:
             _check_turn_numbers(data_points, data_types)
         else:
-            data_points = interpolate_input(data_points, data_types, interpolation)
+            data_points = _interpolate_input(data_points, data_types, 
+                                             interpolation)
 
         if len(data_types) == 1:
             bunching = 'single_bunch'
@@ -99,20 +118,39 @@ class _beam_data(_function):
 
 class acceptance(_beam_data):
     """
-    Class defining the acceptance of the RF bucket
+    Class defining the acceptance of the RF bucket.
+
     Parameters
     ----------
     *args : float, 1D iterable of floats, 2D iterable of floats
-    units : str
-    time : iterable of floats
-    n_turns : int
-    interpolation : str
+        The data defining the programs
+    units : str, optional
+        The units of the data.  The default is 'eVs'
+    time : iterable of floats, optional
+        The time array to be used for the data, to be used if the time 
+        dependent data is not given as 2D array.  The default is None
+    n_turns : int, option
+        The number of turns to be used, if a single value is given it will be
+        extended to n_turns length.  The default is None.
+    interpolation : str, optional
+        The type of interpolation to be used.  The default is 'linear'.
+    **kwargs : keyword arguments
+        Additional kwargs used to define the data_type dict.
     
     Attributes
     ----------
-    As _function plus
+    data_type : dict
+        Dictionary containing relevant information to define the datatype.
+    timebase : str
+        Either 'single', 'by_turn', or 'by_time' depending on the definition
+        of the datatype.  As a string it is used as a key for the data_type 
+        dict.
     units : str
-    bunching : str
+        Identifies the units of the stored data.  Used as a string key in the
+        data_type dict.
+    interpolation : str
+        Identifier of the type of interpolation to be used when reshaping
+        the array, currently only 'linear' has been implemented.
     """
     
     def __new__(cls, *args, units = 'eVs', time = None, n_turns = None, 
@@ -125,22 +163,45 @@ class acceptance(_beam_data):
 
 class emittance(_beam_data):
     """
-    Class defining the longitudinal emittance of the beam
+    Class defining the longitudinal emittance of the bunch.
+
     Parameters
     ----------
     *args : float, 1D iterable of floats, 2D iterable of floats
-    emittance_type : str
-    units : str
-    time : iterable of floats
-    n_turns : int
-    interpolation : str
+        The data defining the programs
+    emittance_type : str, optional
+        The emittance type of the measurement (e.g. 90%, matched area, RMS).
+        The default is 'matched_area'.
+    units : str, optional
+        The units of the data.  The default is 'eVs'
+    time : iterable of floats, optional
+        The time array to be used for the data, to be used if the time 
+        dependent data is not given as 2D array.  The default is None
+    n_turns : int, option
+        The number of turns to be used, if a single value is given it will be
+        extended to n_turns length.  The default is None.
+    interpolation : str, optional
+        The type of interpolation to be used.  The default is 'linear'.
     
     Attributes
     ----------
-    As _function plus
-    units : str
-    bunching : str
+    data_type : dict
+        Dictionary containing relevant information to define the datatype.
+    timebase : str
+        Either 'single', 'by_turn', or 'by_time' depending on the definition
+        of the datatype.  As a string it is used as a key for the data_type 
+        dict.
     emittance_type : str
+        Identifies the type of emittance defined by the data (e.g. 90%, 
+                                                              matched area
+                                                              RMS).  Used as
+        a string key in the data_type dict.
+    units : str
+        Identifies the units of the stored data.  Used as a string key in the
+        data_type dict.
+    interpolation : str
+        Identifier of the type of interpolation to be used when reshaping
+        the array, currently only 'linear' has been implemented.
     """
     
     def __new__(cls, *args, emittance_type = 'matched_area', units = 'eVs', 
@@ -172,22 +233,45 @@ class emittance(_beam_data):
 
 class length(_beam_data):
     """
-    Class defining the length of the beam
+    Class defining the bunch length.
+
     Parameters
     ----------
     *args : float, 1D iterable of floats, 2D iterable of floats
-    length_type : str
-    units : str
-    time : iterable of floats
-    n_turns : int
-    interpolation : str
+        The data defining the programs
+    length_type : str, optional
+        The emittance type of the measurement (e.g. 4 sigma, full length, RMS).
+        The default is 'matched_area'.
+    units : str, optional
+        The units of the data.  The default is 's'
+    time : iterable of floats, optional
+        The time array to be used for the data, to be used if the time 
+        dependent data is not given as 2D array.  The default is None
+    n_turns : int, option
+        The number of turns to be used, if a single value is given it will be
+        extended to n_turns length.  The default is None.
+    interpolation : str, optional
+        The type of interpolation to be used.  The default is 'linear'.
     
     Attributes
     ----------
-    As _function plus
-    units : str
-    bunching : str
+    data_type : dict
+        Dictionary containing relevant information to define the datatype.
+    timebase : str
+        Either 'single', 'by_turn', or 'by_time' depending on the definition
+        of the datatype.  As a string it is used as a key for the data_type 
+        dict.
     length_type : str
+        Identifies the type of emittance defined by the data (e.g. 4 sigma, 
+                                                              full length, 
+                                                              RMS).  Used as
+        a string key in the data_type dict.
+    units : str
+        Identifies the units of the stored data.  Used as a string key in the
+        data_type dict.
+    interpolation : str
+        Identifier of the type of interpolation to be used when reshaping
+        the array, currently only 'linear' has been implemented.
     """
     
     def __new__(cls, *args, length_type = 'full_length', units = 's',
@@ -218,22 +302,44 @@ class length(_beam_data):
 
 class height(_beam_data):
     """
-    Class defining the height of the beam
+    Class defining the bunch height in energy.
+
     Parameters
     ----------
     *args : float, 1D iterable of floats, 2D iterable of floats
-    height_type : str
-    units : str
-    time : iterable of floats
-    n_turns : int
-    interpolation : str
+        The data defining the programs
+    height_type : str, optional
+        The emittance type of the measurement (e.g. half height, full height).
+        The default is 'half height'.
+    units : str, optional
+        The units of the data.  The default is 'eV'
+    time : iterable of floats, optional
+        The time array to be used for the data, to be used if the time 
+        dependent data is not given as 2D array.  The default is None
+    n_turns : int, option
+        The number of turns to be used, if a single value is given it will be
+        extended to n_turns length.  The default is None.
+    interpolation : str, optional
+        The type of interpolation to be used.  The default is 'linear'.
     
     Attributes
     ----------
-    As _function plus
-    units : str
-    bunching : str
+    data_type : dict
+        Dictionary containing relevant information to define the datatype.
+    timebase : str
+        Either 'single', 'by_turn', or 'by_time' depending on the definition
+        of the datatype.  As a string it is used as a key for the data_type 
+        dict.
     height_type : str
+        Identifies the type of emittance defined by the data (e.g. half height,
+                                                              full height).  
+        Used as a string key in the data_type dict.
+    units : str
+        Identifies the units of the stored data.  Used as a string key in the
+        data_type dict.
+    interpolation : str
+        Identifier of the type of interpolation to be used when reshaping
+        the array, currently only 'linear' has been implemented.
     """
     
     def __new__(cls, *args, height_type = 'half_height', units = 'eV',
@@ -263,20 +369,37 @@ class height(_beam_data):
 
 class synchronous_phase(_beam_data):
     """
-    Class defining the length of the beam
+    Class defining the synchronous phase.
+
     Parameters
     ----------
     *args : float, 1D iterable of floats, 2D iterable of floats
-    units : str
-    time : iterable of floats
-    n_turns : int
-    interpolation : str
+        The data defining the programs
+    units : str, optional
+        The units of the data.  The default is 's'
+    time : iterable of floats, optional
+        The time array to be used for the data, to be used if the time 
+        dependent data is not given as 2D array.  The default is None
+    n_turns : int, option
+        The number of turns to be used, if a single value is given it will be
+        extended to n_turns length.  The default is None.
+    interpolation : str, optional
+        The type of interpolation to be used.  The default is 'linear'.
     
     Attributes
     ----------
-    As _function plus
+    data_type : dict
+        Dictionary containing relevant information to define the datatype.
+    timebase : str
+        Either 'single', 'by_turn', or 'by_time' depending on the definition
+        of the datatype.  As a string it is used as a key for the data_type 
+        dict.
     units : str
-    bunching : str
+        Identifies the units of the stored data.  Used as a string key in the
+        data_type dict.
+    interpolation : str
+        Identifier of the type of interpolation to be used when reshaping
+        the array, currently only 'linear' has been implemented.
     """
     
     def __new__(cls, *args, units = 's', time = None, n_turns = None, 
