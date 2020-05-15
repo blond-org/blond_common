a332c6e14c5c22909c677d4844229ed0ea30d9c6
Simon Albright <simon.albright@cern.ch>
2020-05-15 15:00:29 +0200

    added check for length of well in _interp_from_potential

--- beam_dynamics/bucket.py
+++ beam_dynamics/bucket.py
@@ -315,6 +315,8 @@ class Bucket:
             raise excpt.InputError("Target potential must be positive")
         
         pts = np.where(self.well <= potential)[0]
+        if len(pts) < 2:
+            raise excpt.InputError("Too few points below target potential")
         leftPt = pts[0]
         rightPt = pts[-1]
 
@@ -413,7 +415,8 @@ class Bucket:
 
             nPts = args[0]
             try:
-                interpTime = self._interp_time_from_potential(potential[0], nPts)
+                interpTime = self._interp_time_from_potential(potential[0], 
+                                                              nPts)
             except excpt.InputError:
                 return self.area
             
@@ -429,7 +432,7 @@ class Bucket:
             emittance = 2*np.trapz(energyContour, interpTime)
             
             return np.abs(target_emittance - emittance)
-    
+        
         result = opt.minimize(emit_func, np.max(self.well)/2, 
                               method='Nelder-Mead', args=(nPts,))
 
