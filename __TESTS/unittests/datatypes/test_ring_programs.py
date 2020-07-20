6d4855d37abcd062aab89177b5ac81ca5481d2dd
Simon Albright <simon.albright@cern.ch>
2020-07-20 16:22:04 +0200

    starting tests for ring_programs

---
+++ __TESTS/unittests/datatypes/test_ring_programs.py
@@ -0,0 +1,35 @@
+# coding: utf8
+# Copyright 2019 CERN. This software is distributed under the
+# terms of the GNU General Public Licence version 3 (GPL Version 3),
+# copied verbatim in the file LICENCE.md.
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+# Project website: http://blond.web.cern.ch/
+
+'''
+Test dataypes._core.py
+
+'''
+
+# General imports
+# ---------------
+import sys
+import unittest
+import numpy as np
+import numpy.testing as npTest
+import os
+
+this_directory = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+# BLonD_Common imports
+# --------------------
+if os.path.abspath(this_directory + '../../../../') not in sys.path:
+    sys.path.insert(0, os.path.abspath(this_directory + '../../../../'))
+
+import blond_common.datatypes._core as core
+from blond_common.devtools import exceptions
+
+class test_ring_programs(unittest.TestCase):
+
+    
