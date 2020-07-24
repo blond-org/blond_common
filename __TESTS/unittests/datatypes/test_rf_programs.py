43e72e2f3f5b7571613b7a1c1ab6cf9dd7a42be9
Simon Albright <simon.albright@cern.ch>
2020-07-24 11:58:38 +0200

    created test file

---
+++ __TESTS/unittests/datatypes/test_rf_programs.py
@@ -0,0 +1,32 @@
+# coding: utf8
+# Copyright 2019 CERN. This software is distributed under the
+# terms of the GNU General Public Licence version 3 (GPL Version 3),
+# copied verbatim in the file LICENCE.md.
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+# Project website: http://blond.web.cern.ch/
+
+'''
+Test dataypes._core.py
+
+'''
+
+# General imports
+# ---------------
+import sys
+import unittest
+import numpy as np
+import numpy.testing as npTest
+import os
+import scipy.constants as cont
+
+this_directory = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+# BLonD_Common imports
+# --------------------
+if os.path.abspath(this_directory + '../../../../') not in sys.path:
+    sys.path.insert(0, os.path.abspath(this_directory + '../../../../'))
+
+import blond_common.datatypes.rf_programs as rfProg
+from blond_common.devtools import exceptions
