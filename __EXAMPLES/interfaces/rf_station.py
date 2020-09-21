1b8017012a0d01e3c203c1a7ac2818279068ad5a
alasheen <alexandre.lasheen@cern.ch>
2020-09-21 15:49:40 +0200

    Adding more complex examples

--- __EXAMPLES/interfaces/rf_station.py
+++ __EXAMPLES/interfaces/rf_station.py
@@ -48,7 +48,48 @@ rf_station = RFStation(ring,
 
 # rf_station = RFStation.direct_input()
 
-print('-- Simple input, rf harmonic')
+print('-- Complex input 1')
+print(f'RF voltage {rf_station.voltage} [V]')
+print(f'RF phase {rf_station.phi_rf} [rad]')
+print(f'RF harmonic {rf_station.harmonic}')
+print(f'RF frequency {rf_station.f_rf} [Hz]')
+print()
+
+
+# To declare many RF systems with time based programs, and combining
+# them into one representing the total voltage seen by the beam
+
+harmonic_1 = [[0, 1, 2], [2, 2, 2]]  # [s, unitless]
+voltage_1 = [[0, 1.5, 2], [1e3, 1.1e3, 1.2e3]]  # [s, V]
+phase_1 = np.pi  # rad
+
+harmonic_2 = [[0, 1, 2], [2, 2, 2]]  # [s, unitless]
+voltage_2 = [[0, 1, 2], [1e3, 1.1e3, 1.2e3]]  # [s, V]
+phase_2 = [[0, 0.5, 2], [np.pi, np.pi - 0.1, np.pi - 0.2]]  # [s, rad]
+
+harmonic_3 = [[0, 1, 2], [2, 3, 3]]  # [s, unitless]
+voltage_3 = [[0, 1, 2], [0.5e3, 1.0e3, 1.5e3]]  # [s, V]
+phase_3 = [[0, 1.5, 2], [0., 0.1, 0.2]]  # [s, rad]
+
+freq_4 = [[0, 1, 2], [100e3, 100.1e3, 100.2e3]]  # [s, Hz]
+voltage_4 = [[0, 1, 2], [0.5e3, 1.0e3, 1.5e3]]  # [s, V]
+phase_4 = [[0, 1.5, 2], [0., 0.1, 0.2]]  # [s, rad]
+
+rf_system_1 = RFSystem(voltage_1, phase_1, harmonic_1)
+rf_system_2 = RFSystem(voltage_2, phase_2, harmonic_2)
+rf_system_3 = RFSystem(voltage_3, phase_3, harmonic_3)
+rf_system_4 = RFSystem(voltage_4, phase_4, frequency=freq_4)
+
+rf_station = RFStation(ring,
+                       [rf_system_1,
+                        rf_system_2,
+                        rf_system_3,
+                        rf_system_4],
+                       combine_systems=True)
+
+# rf_station = RFStation.direct_input()
+
+print('-- Complex input 2')
 print(f'RF voltage {rf_station.voltage} [V]')
 print(f'RF phase {rf_station.phi_rf} [rad]')
 print(f'RF harmonic {rf_station.harmonic}')
