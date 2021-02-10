951dd58c24090e4f7e5e46faeb603f9fb112425b
alasheen <alexandre.lasheen@cern.ch>
2021-02-10 16:43:02 +0100

    Catching warning in unittest that is covered by specific unittest

--- __TESTS/unittests/interfaces/machine_parameters/test_ring_section.py
+++ __TESTS/unittests/interfaces/machine_parameters/test_ring_section.py
@@ -302,6 +302,13 @@ class TestRingSection(unittest.TestCase):
                 length, section.length)
 
         with self.subTest('Time based program - Only orbit length'):
+
+            warnings.filterwarnings(
+                "ignore",
+                message='The synchronous data was defined as single ' +
+                'element while the input orbit_bump was defined turn ' +
+                'or time based. Only the first element of the program ' +
+                'will be taken in the Ring object after treatment.')
             section = RingSection(length, alpha_0[1][0], momentum[1][0],
                                   orbit_bump=orbit_bump)
             # The warning raised here is expected and treated by a specific
@@ -320,6 +327,13 @@ class TestRingSection(unittest.TestCase):
                 section.length[0, :, :])
 
         with self.subTest('Time based program - Only momentum compaction'):
+            
+            warnings.filterwarnings(
+                "ignore",
+                message='The synchronous data was defined as single ' +
+                'element while the input alpha_0 was defined turn or ' +
+                'time based. Only the first element of the program will ' +
+                'be taken in the Ring object after treatment.')
             section = RingSection(length, alpha_0, momentum[1][0])
             # The warning raised here is expected and treated by a specific
             # unittest
